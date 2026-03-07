be65dc0f5f940287c8e6ec5cd40ae68ee359a928
vaneet44 <vaneet44@gmail.com>
2026-03-07 22:29:44 +0530

    Move industries into landing page section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import Navigation from './sections/Navigation';
-import HeroSection from './sections/HeroSection';
-import BookingSection from './sections/BookingSection';
-import PaymentSection from './sections/PaymentSection';
-import CalendarSection from './sections/CalendarSection';
-import OwnerSection from './sections/OwnerSection';
-import ClosingSection from './sections/ClosingSection';
 import HomePage from '@/pages/HomePage';
-import IndustriesPage from '@/pages/IndustriesPage';
 
 import { useIndustryMetadata } from './hooks/useIndustryMetadata';
 
@@ -18,8 +10,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 function App() {
-  const pathname = window.location.pathname.replace(/\/$/, '') || '/';
-
   useIndustryMetadata();
 
   useEffect(() => {
@@ -27,10 +17,6 @@ function App() {
       ScrollTrigger.getAll().forEach(st => st.kill());
     };
   }, []);
-  if (pathname === '/industries') {
-    return <IndustriesPage />;
-  }
-
   return <HomePage />;
 }
 
