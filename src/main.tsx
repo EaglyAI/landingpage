9e92deba2668912af8fa330d215627213e349023
Puneet Goyal <puneetgoyal08@gmail.com>
2026-03-07 17:10:10 +0530

    Force new Pages bundle hash to bypass cached asset 404

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Forces a new production bundle hash when redeploying Pages.
+document.documentElement.setAttribute('data-release', '2026-03-07-01')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
